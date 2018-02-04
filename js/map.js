23ef66ad94bcfda0c3939542b83eb6ed05ba8c44
jaime <garciadiazjaime@gmail.com>
2018-02-03 21:07:47 -0800

    add bower

--- js/map.js
+++ js/map.js
@@ -1,22 +1,28 @@
+const center = [37.7599904,-122.4536476]
+
 const map = tomtom.map('map', {
+  center,
+  zoom: 14,
   key: 'V6iEh5xVqo3mBAHEe3reZYZG10yAhbBt',
   source: 'vector',
   basePath: '/tomtomSDK',
 })
 
-const bounds = [
-  [37.798228, -122.518443],
-  [37.704575, -122.343863]
-]
 
-const markers = [
-  [37.7568453, -122.4578464],
-  [37.7599043, -122.4256016],
-  [37.7610579, -122.4094225]
-]
 
-markers.forEach(marker => tomtom.L.marker(marker).addTo(map))
-map.fitBounds(bounds)
+// const bounds = [
+//   [37.798228, -122.518443],
+//   [37.704575, -122.343863]
+// ]
+
+// const markers = [
+//   [37.7568453, -122.4578464],
+//   [37.7599043, -122.4256016],
+//   [37.7610579, -122.4094225]
+// ]
+
+// markers.forEach(marker => tomtom.L.marker(marker).addTo(map))
+// map.fitBounds(bounds)
 
 tomtom.controlPanel({
     position: 'bottomright',
