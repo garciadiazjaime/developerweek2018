817fcaf528f21a31c9a5024afe41081cdd8b85f1
jaime <garciadiazjaime@gmail.com>
2018-02-03 18:36:34 -0800

    separete js/css

--- src/js/index.js
+++ src/js/index.js
@@ -1,55 +1,50 @@
-(function(document) {
+(function (document) {
   'use strict';
 
   function $(selector) {
-      return document.querySelector(selector);
+    return document.querySelector(selector);
   }
 
-  window.addEventListener('WebComponentsReady', function() {
+  window.addEventListener('WebComponentsReady', () => {
+    const textInput = $('#textInput');
+    const writeHere = $('.write-here');
 
-      var textInput = $('#textInput');
-      var textButton = $('#textButton');
-      var writeHere = $('.write-here');
+    if (textInput.applicationkey === "REPLACE_ME" || textInput.hmackey === "REPLACE_ME") {
+      $('header').remove();
+      $('.warningkeys').classList.remove('hidden');
+    } else {
+      const listOfInputs = [textInput];
+      listOfInputs.forEach(function (input) {
+        input.addEventListener('loaded', () => {
+          console.log('mint:loaded')
+          writeHere.classList.remove('hidden');
+        });
+        input.addEventListener('pointerdown', () => {
+          console.log('mint:pointerdown')
+          writeHere.classList.add('hidden');
+        });
+        input.addEventListener('pointerup', () => {
+          console.log('mint:pointerup')
+          writeHere.classList.add('hidden');
+        });
+      });
+      writeHere.addEventListener('pointermove', () => {
+        writeHere.classList.add('hidden');
+      });
 
-      if (textInput.applicationkey === "REPLACE_ME" || textInput.hmackey === "REPLACE_ME" ) {
-          $('header').remove();
-          $('.warningkeys').classList.remove('hidden');
-      } else {
+      const setButtonsStates = () => {
+        writeHere.classList.add('hidden');
+        listOfInputs.forEach(function (input) {
+          input.classList.add('hidden');
+        });
 
-          var listOfInputs = [textInput];
-          listOfInputs.forEach(function(input) {
-              input.addEventListener('loaded', function() {
-                  console.log('mint:loaded')
-                  writeHere.classList.remove('hidden');
-              });
-              input.addEventListener('pointerdown', function() {
-                  console.log('mint:pointerdown')
-                  writeHere.classList.add('hidden');
-              });
-              input.addEventListener('pointerup', function() {
-                  console.log('mint:pointerup')
-                  writeHere.classList.add('hidden');
-              });
-          });
-          writeHere.addEventListener('pointermove', function() {
-              writeHere.classList.add('hidden');
-          });
+        textInput.classList.remove('hidden');
+        textInput.removeAttribute('unloaded');
+      };
 
-          // Manage the tap on the various buttons
-          var setButtonsStates = function(isTextButton) {
-              writeHere.classList.add('hidden');
-              listOfInputs.forEach(function(input) { input.classList.add('hidden'); });
-
-              textButton.active = isTextButton;
-              if (isTextButton) {
-                  textInput.classList.remove('hidden');
-                  textInput.removeAttribute('unloaded');
-              }
-          };
-
-          // // Initialize the default demo
-          setButtonsStates(true, false);
-      }
+      // // Initialize the default demo
+      setButtonsStates();
+    }
   });
 
 })(document);
